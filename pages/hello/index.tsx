3a53ed6c47d4c724e1d6c47769e16eb46ce605a4
bk <burakkaraoglan55@gmail.com>
2023-01-12 17:08:20 +0300

    init server props

--- pages/hello/index.tsx
+++ pages/hello/index.tsx
@@ -1,14 +1,43 @@
 import useSWR from 'swr';
 import axios from "axios";
+import {GetServerSideProps, NextPage} from "next";
+import {processEnv} from "@next/env";
 
-export default function Index(name: string) {
-  const address = `/api/hello`;
+interface HomeProps {
+  name: string;
+}
+
+const Index: NextPage<HomeProps> = ({name}) => {
   const fetcher = async (url: string) => await axios.get(url).then((res) => res.data);
 
-  const { data, error, isLoading } = useSWR('/api/hello', fetcher)
+  const {data, error, isLoading} = useSWR('/api/hello', fetcher)
 
   if (error) return <div>Failed to load</div>
   if (isLoading) return <div>Loading...</div>
 
-  return (<h2>selamlar nasılsınız? --- {data.name}</h2>);
+  return (<div>
+    <h2>selamlar nasılsınız? --- from swr fetcher {data.name}</h2>
+    <h3>from init props -- {name}</h3>
+  </div>);
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  console.log('ENVENEN', process.env.ENV)
+  try {
+    const { data } = await axios.get<HomeProps>(
+      `http://localhost:3000/api/hello`,
+    );
+    console.log('NAME', data);
+    return { props: { name: data.name } };
+  } catch (error) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: process.env.NEXT_PUBLIC_HOMEPAGE_URL,
+      },
+      props: {},
+    };
+  }
+}
+
+export default Index;
